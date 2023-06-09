4bf073f0ee9bc76dc76fa8f960285059a7fc923a
cam273364 <cam273364@gmail.com>
2023-06-09 16:23:35 -0400

    'added jawsdb backup for heroku'

--- config/index.js
+++ config/index.js
@@ -1,12 +1,17 @@
 require("dotenv").config()
 const Sequelize = require('sequelize')
 
-const{ DB_NAME1, DB_USER, DB_PASSWORD } = process.env
-console.log(process.env.ABC)
-const sequelize = new Sequelize(DB_NAME1, DB_USER, DB_PASSWORD, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-})
+const{ DB_NAME1, DB_USER, DB_PASSWORD, JAWSDB_URL } = process.env
+let sequelize
+if(JAWSDB_URL) {
+    sequelize = new Sequelize(JAWSDB_URL)
+} else {
+     sequelize = new Sequelize(DB_NAME1, DB_USER, DB_PASSWORD, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    })
+}
+
 
 module.exports = sequelize
